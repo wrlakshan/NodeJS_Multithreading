b6b4a1c1a99f0d073d173e79a9bc126f90d62642
wrlakshan <rashmika.lakshan@ebuilder.com>
2024-02-20 18:37:17 +0530

    worker thread

--- index.js
+++ index.js
@@ -1,10 +1,45 @@
 const express = require("express");
+const { Worker, workerData } = require("worker_threads");
 
 const app = express();
 const port = process.env.PORT || 3000;
+const THREAD_COUNT = 12;
+
+function createWorker() {
+  return new Promise((resolve, reject) => {
+    const worker = new Worker("./worker.js", {
+      workerData: { thread_count: THREAD_COUNT },
+    });
+
+    worker.on("message", (data) => {
+      resolve(data);
+    });
+
+    worker.on("error", (err) => {
+      reject(err);
+    });
+  });
+}
 
 app.get("/non-blocking", (req, res) => {
   res.status(200).send("This page is non-blocking");
+});
+
+app.get("/blocking-thread", async (req, res) => {
+  const workerPromises = [];
+
+  for (let i = 0; i < THREAD_COUNT; i++) {
+    workerPromises.push(createWorker());
+  }
+
+  const thread_results = await Promise.all(workerPromises);
+  const total =
+    thread_results[0] +
+    thread_results[1] +
+    thread_results[2] +
+    thread_results[3];
+
+  res.status(200).send(`results: ${total}`);
 });
 
 app.get("/blocking", (req, res) => {
---
+++ worker.js
@@ -0,0 +1,8 @@
+const { workerData, parentPort } = require("worker_threads");
+
+let count = 0;
+for (let i = 0; i < 20_000_000_000 / workerData.thread_count; i++) {
+  count++;
+}
+
+parentPort.postMessage(count);
